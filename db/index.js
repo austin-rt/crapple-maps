33decf1f7e049d41c663b192d778eaa4f242372e
Austin Taylor <austinrtaylor91@gmail.com>
2022-07-28 23:03:32 -0400

    attempting heroku deploy

--- db/index.js
+++ db/index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/<database_name>';
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/crappleMapsDatabase')
+  .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB.');
+    console.log('Successfully connected to MongoDB!');
   })
   .catch((e) => {
     console.error('Connection error', e.message);
